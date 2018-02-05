7aa9f63adbb5e7dede3dbf57509198f9bb341e83
Fabio Buso <dev.siroibaf@gmail.com>
2018-02-05 14:16:12 +0100

    No home

--- setup_svc.sh
+++ setup_svc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -f $HOME/VBoxSVC.pid ];
+if [ -f /home/jenkins/VBoxSVC.pid ];
 then
-  PSOUT=$(ps aux | grep $(cat $HOME/VBoxSVC.pid) | wc -l)
+  PSOUT=$(ps aux | grep $(cat /home/jenkins/VBoxSVC.pid) | wc -l)
   if [ $PSOUT -eq 1 ];
   then
     # The vm daemon is not up, start a new one
-    nohup /usr/lib/virtualbox/VBoxSVC --pidfile $HOME/VBoxSVC.pid >/dev/null 2>&1 &
+    nohup /usr/lib/virtualbox/VBoxSVC --pidfile /home/jenkins/VBoxSVC.pid >/dev/null 2>&1 &
   fi
 else
-    nohup /usr/lib/virtualbox/VBoxSVC --pidfile $HOME/VBoxSVC.pid >/dev/null 2>&1 &
+    nohup /usr/lib/virtualbox/VBoxSVC --pidfile /home/jenkins/VBoxSVC.pid >/dev/null 2>&1 &
 fi
