8cceb846081c50f4ae8b263231296dd02fb81d0d
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2018-10-03 15:33:08 +0200

    [test_HOPSWORKS-582]  test

--- scripts/clone_karamel_chef.sh
+++ scripts/clone_karamel_chef.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ -d karamel-chef ]; then
-  cd karamel-chef
-  git pull
-  cd ..
-else
-  git clone "https://github.com/hopshadoop/karamel-chef"
-fi
+# if [ -d karamel-chef ]; then
+#   cd karamel-chef
+#   git pull
+#   cd ..
+# else
+#   git clone "https://github.com/hopshadoop/karamel-chef"
+# fi
+git clone "https://github.com/kouzant/karamel-chef.git"
+cd karamel-chef
+git checkout HOPSWORKS-582
+cd ..
