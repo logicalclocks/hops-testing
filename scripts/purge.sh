21d7e64571bd0d8bb0ee09e309a851315b32c4ac
Fabio Buso <buso.fabio@gmail.com>
2019-01-25 18:47:25 +0100

    Purge hostonlyifs

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -4,6 +4,7 @@ set -e
 VBOX_MANAGE=/usr/bin/vboxmanage
 
 vms=`$VBOX_MANAGE list vms | awk -F'[{|}]' '{print $2}'`
+privnetif=`${VBOX_MANAGE} showvminfo hopsworks1 | grep 'Host-only Interface' | awk -F',' '{print $2}' | awk -F' ' '{print $4}' | sed "s/'//g"`
 
 pkill VBoxHeadless
 sleep 10
@@ -11,6 +12,8 @@ sleep 10
 for vm in ${vms}; do
   $VBOX_MANAGE unregistervm ${vm}
 done
+
+$VBOX_MANAGE hostonlyif remove $privnetif
 
 rm -rf $HOME/VirtualBox\ VMs/*
 rm -rf $HOME/.berkshelf
