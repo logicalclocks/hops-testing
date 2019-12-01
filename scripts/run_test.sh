2560bcfc08cdf59e32e9db45cbb9c05c11564460
Fabio Buso <buso.fabio@gmail.com>
2019-12-02 00:06:22 +0100

    rename vms

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -10,13 +10,14 @@ if [ "$1" == "ubuntu" ]; then
 
   cp templates/Vagrantfile-ubuntu karamel-chef/Vagrantfile
   cp templates/cluster-ubuntu karamel-chef/cluster.yml
+  sed -i "s/ubuntu-name/ubuntu-$BUILD_NUMBER/g" karamel-chef/Vagrantfile
 else
   # Centos
   cp templates/Vagrantfile-centos karamel-chef/Vagrantfile
   cp templates/cluster-centos karamel-chef/cluster.yml
 fi
 
-sed -i "s/test_platform/$BUILD_NUMBER/g" karamel-chef/cluster.yml
+sed -i "s/centos-name/centos-$BUILD_NUMBER/g" karamel-chef/Vagrantfile
 
 # Execute the tests
 cd karamel-chef
