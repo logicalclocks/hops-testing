f9df427d01807b11a2c29fadee7fd8bb42122028
Fabio Buso <buso.fabio@gmail.com>
2019-12-03 09:45:28 +0100

    fix check_licenses to use

--- scripts/check_licenses.sh
+++ scripts/check_licenses.sh
@@ -16,4 +16,4 @@ fi
 cd repos
 
 # Currently we only check the license headers in Hopsworks
-python $WORKSPACE/scripts/license_checker.py --dir hopsworks --whitelist_dir $WORKSPACE/licenses_whitelist --branch test_platform --fork_commit ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b --parallelism 12
+python $WORKSPACE/scripts/license_checker.py --dir hopsworks --whitelist_dir $WORKSPACE/licenses_whitelist --branch $BUILD_NUMBER --fork_commit ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b --parallelism 12
