38b004456293ac6354d718777b50b3019a54d164
Fabio Buso <buso.fabio@gmail.com>
2020-02-04 23:06:34 +0100

    Make sure origin is up to date on the worker doing the license checking

--- scripts/check_licenses.sh
+++ scripts/check_licenses.sh
@@ -13,7 +13,10 @@ else
 fi
 
 # All the repositories are in the repos directory under the workspace directory
-cd repos
+cd repos/hopsworks
+
+# Make sure origin is up to date in this directory
+git fetch origin
 
 # Currently we only check the license headers in Hopsworks
 python $WORKSPACE/scripts/license_checker.py --dir hopsworks --whitelist_dir $WORKSPACE/licenses_whitelist --branch $BUILD_NUMBER --fork_commit ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b --parallelism 12
