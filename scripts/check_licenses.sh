6fa389f53d00e0810dcc37506b9afbfe0e1e7c06
Fabio Buso <buso.fabio@gmail.com>
2020-02-04 23:10:35 +0100

    fix input path for license checker

--- scripts/check_licenses.sh
+++ scripts/check_licenses.sh
@@ -19,4 +19,4 @@ cd repos/hopsworks
 git fetch origin
 
 # Currently we only check the license headers in Hopsworks
-python $WORKSPACE/scripts/license_checker.py --dir hopsworks --whitelist_dir $WORKSPACE/licenses_whitelist --branch $BUILD_NUMBER --fork_commit ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b --parallelism 12
+python $WORKSPACE/scripts/license_checker.py --dir . --whitelist_dir $WORKSPACE/licenses_whitelist --branch $BUILD_NUMBER --fork_commit ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b --parallelism 12
