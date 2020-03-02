3782846c1019ff5e7e7bf7d7ee5ba13bd55ea823
Antonios Kouzoupis <kouzoupis.ant@gmail.com>
2020-03-02 14:01:15 +0100

    [test_HOPSWORKS-1405] Add consul-chef to cloned repos

--- scripts/prepare_repos.sh
+++ scripts/prepare_repos.sh
@@ -6,7 +6,8 @@ repos=(hopsworks hopsworks-chef conda-chef kagent-chef hops-hadoop-chef
 	   spark-chef flink-chef livy-chef ndb-chef
 	   dr-elephant-chef tensorflow-chef epipe-chef dela-chef
 	   kzookeeper kafka-cookbook elasticsearch-chef hopslog-chef
-	   hopsmonitor-chef chef-glassfish chef-ulimit hive-chef airflow-chef)
+	   hopsmonitor-chef chef-glassfish chef-ulimit hive-chef airflow-chef
+           consul-chef)
 
 # Create a cookbook directory - if it doesn't exists
 if [ ! -d repos ];
