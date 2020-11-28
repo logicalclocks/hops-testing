c908b4a480fd2f847c5240c13be69653c338eb68
Fabio Buso <buso.fabio@gmail.com>
2020-11-28 18:26:44 +0100

    shutdown script improvement

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-virsh undefine karamel-chef_$1
-virsh destroy karamel-chef_$1
+set -x
+
+export LIBVIRT_DEFAULT_URI="qemu:///system"
+
+virsh undefine "karamel-chef_$1"
+virsh destroy "karamel-chef_$1"
 
 # don't fail the script if the machines are already down
 exit 0
