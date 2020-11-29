df7b1bd10f06ff64347511955913918048296de1
Fabio Buso <buso.fabio@gmail.com>
2020-11-29 15:58:51 +0100

    remove HDs when shutting down the machines

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -7,5 +7,11 @@ export LIBVIRT_DEFAULT_URI="qemu:///system"
 virsh undefine "karamel-chef_$1"
 virsh destroy "karamel-chef_$1"
 
+rm -rf /home/fabio/libvirt/images/karamel-chef_$1.img
+
+# Centos also has a separate HD
+rm -rf /home/fabio/libvirt/images/$1.img
+
+
 # don't fail the script if the machines are already down
 exit 0
